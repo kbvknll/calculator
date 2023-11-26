6f3fe5896bc2d1677b2d2b953a038e78ea31e01d
Екатерина Гордеева <katiyhagordeeva@mail.ru>
2023-11-26 11:04:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Введите свое выражение: ");
+                    string Expression = Console.ReadLine();
+                    Expression = Expression.Replace('\t', '\0');
+                    Console.WriteLine("Ответ: " + dt.Compute(Expression, string.Empty));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ошибкааа!!!");
+            }
+
+        }
+
+    }
+}
